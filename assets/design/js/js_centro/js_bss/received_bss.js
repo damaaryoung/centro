ec56ca1a6a42a0b69bd7fe0f6d595654afd66da8
rekam sari <staf_tidev9@kreditmandiri.co.id>
2021-04-21 10:54:03 +0700

    Update button prosess assign to kolektor di halaman received bss

--- assets/design/js/js_centro/js_bss/received_bss.js
+++ assets/design/js/js_centro/js_bss/received_bss.js
@@ -310,18 +310,21 @@ $('#TableReceived').on('click', '.btn-ok', function () {
 
 // send approval from HEAD OPERASIONAL to KOLEKTOR
 $('#TableReceived').on('click', '.btn-approval-kolektor', function () {
-  let data = {
-    id: this.getAttribute('data-id'),
-    no_awal: $(this).parents("tr").find('td.no_awal').text(),
-    no_akhir: $(this).parents("tr").find('td.no_akhir').text(),
-    nama_user_send: $(this).parents("tr").find('td.nama_user_send').text(),
-    is_migrasi: this.getAttribute('migrasi'),
-    user_id_received: this.getAttribute('user_id_received'),
-    kode_kantor_received: this.getAttribute('kode_kantor_received'),
-    appoved: this.getAttribute('appoved')
+  if (confirm("Konfirmasi,Yakin Anda ingin Assign ke Kolektor ??") == true) {
+    let data = {
+      id: this.getAttribute('data-id'),
+      no_awal: $(this).parents("tr").find('td.no_awal').text(),
+      no_akhir: $(this).parents("tr").find('td.no_akhir').text(),
+      nama_user_send: $(this).parents("tr").find('td.nama_user_send').text(),
+      is_migrasi: this.getAttribute('migrasi'),
+      user_id_received: this.getAttribute('user_id_received'),
+      kode_kantor_received: this.getAttribute('kode_kantor_received'),
+      appoved: this.getAttribute('appoved')
+    }
+    insertReceived(data)
+  } else {
+    return false;
   }
-
-  insertReceived(data)
 })
 
 //send approval ok to query
